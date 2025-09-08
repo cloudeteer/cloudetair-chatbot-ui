5978ccf5ef767afbd4742b3ebb45c578a243effa
Roman Schwarz <rs@cloudeteer.de>
2025-09-08 14:28:57 +0200

    Refactor domain URL construction logic in entrypoint script
    Signed-off-by: Roman Schwarz <rs@cloudeteer.de>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,15 @@
 # and contains the fully qualified domain name (FQDN) of this container instance.
 
 # Set DOMAIN_CLIENT and DOMAIN_SERVER only if they are not already defined
-# and if CONTAINER_APP_HOSTNAME is available.
-if [ -z "$DOMAIN_CLIENT" ] && [ -n "$CONTAINER_APP_HOSTNAME" ]; then
-  export DOMAIN_CLIENT="https://$CONTAINER_APP_HOSTNAME"
+# and if CONTAINER_APP_ENV_DNS_SUFFIX and CONTAINER_APP_NAME is available.
+#
+# CONTAINER_APP_ENV_DNS_SUFFIX and CONTAINER_APP_NAME are used to construct the application URL,
+# as CONTAINER_APP_HOSTNAME includes the deployment revision, which is not suitable for OAuth flows.
+if [ -z "$DOMAIN_CLIENT" ] && [ -n "$CONTAINER_APP_ENV_DNS_SUFFIX" ] && [ -n "$CONTAINER_APP_NAME" ]; then
+  export DOMAIN_CLIENT="https://${CONTAINER_APP_NAME}.${CONTAINER_APP_ENV_DNS_SUFFIX}"
 fi
-if [ -z "$DOMAIN_SERVER" ] && [ -n "$CONTAINER_APP_HOSTNAME" ]; then
-  export DOMAIN_SERVER="https://$CONTAINER_APP_HOSTNAME"
+if [ -z "$DOMAIN_SERVER" ] && [ -n "$CONTAINER_APP_ENV_DNS_SUFFIX" ] && [ -n "$CONTAINER_APP_NAME" ]; then
+  export DOMAIN_SERVER="https://${CONTAINER_APP_NAME}.${CONTAINER_APP_ENV_DNS_SUFFIX}"
 fi
 
 # Start the main application process
